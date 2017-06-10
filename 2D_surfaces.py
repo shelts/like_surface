255e6f01c447ba0d164441cc1630dfd7682b75f0
shelts <shelts@rpi.edu>
2017-06-09 21:19:29 -0400

    update

--- 2D_surfaces.py
+++ 2D_surfaces.py
@@ -58,9 +58,9 @@ mr_s, mr_e, mr_in = mr[0], mr[1], mr[2]
 ft_N = 25
 bt_N = 25
 r_N  = 4
-rr_N = 25
+rr_N = 2
 m_N  = 4
-mr_N = 25
+mr_N = 2
 
 
 
@@ -72,9 +72,9 @@ n  = False
 make_folders              = n
 rebuild_binary            = n
 make_correct_answer_hist  = n
-run_regular_iteration     = n
+run_regular_iteration     = y
 run_random_iteration      = n
-
+run_truly_random_iter     = n
 
 
 run_ft_v_bt = n
@@ -97,25 +97,28 @@ run_rr_v_mr = y
 
 run_m_v_mr  = n
 #--------------------------------------------------------------------------------------------------
-class paramaters:
-    def _init_(self, ft_c, bt_c, r_c, rr_c, m_c, mr_c, hist_path, sweep_name, pipe_name):
-    self.ft_c       = ft_c 
-    self.bt_c       = bt_c
-    self.r_c        = r_c
-    self.rr_c       = rr_c
-    self.m_c        = m_c
-    self.mr_c       = mr_c
-    self.hist_path  = hist_path
-    self.sweep_name = sweep_name
-    self.pipe_name  = pipe_name
-    
-    self.ft_tmp       = ft_c 
-    self.bt_tmp       = bt_c
-    self.r_tmp        = r_c
-    self.rr_tmp       = rr_c
-    self.m_tmp        = m_c
-    self.mr_tmp       = mr_c
-    self.hist_tmp     = hist_path
+class parameters:
+    def __init__(self, ft_c, bt_c, r_c, rr_c, m_c, mr_c, hist_path, sweep_name, pipe_name, file_name, parameter1, parameter2):
+        self.ft_c       = ft_c 
+        self.bt_c       = bt_c
+        self.r_c        = r_c
+        self.rr_c       = rr_c
+        self.m_c        = m_c
+        self.mr_c       = mr_c
+        self.hist_path  = hist_path
+        self.sweep_name = sweep_name
+        self.pipe_name  = pipe_name
+        self.file_name  = file_name
+        self.parameter1 = parameter1
+        self.parameter2 = parameter2
+        
+        self.ft_tmp       = ft_c 
+        self.bt_tmp       = bt_c
+        self.r_tmp        = r_c
+        self.rr_tmp       = rr_c
+        self.m_tmp        = m_c
+        self.mr_tmp       = mr_c
+        self.hist_tmp     = hist_path
     
     def set_parameter(self, parameter, value):
         if(parameter == 'ft'):#initializes the needed parameter to the value
@@ -168,7 +171,7 @@ class paramaters:
             
 
     def set_hist(self):
-        self.hist_tmp = self.hist_path + ft_tmp + "_" + bt_tmp + "_" + r_tmp + "_" + rr_tmp + "_" + m_tmp + "_" + mr_tmp + ".hist"
+        self.hist_tmp = self.hist_path + self.ft_tmp + "_" + self.bt_tmp + "_" + self.r_tmp + "_" + self.rr_tmp + "_" + self.m_tmp + "_" + self.mr_tmp + ".hist"
         
     def run_nbody(self):
         output_hist = self.hist_tmp
@@ -188,7 +191,7 @@ class paramaters:
                 -n 14 -b -e " + seed + " -i " + ft + " " + bt + " " + r + " " + rr + " " + m + " " + mr + " \
                 2>>" + folder + "parameter_sweeps" + sweep_name + "/" + file_name + ".txt")
 
-    def write_parameters(self, f, paramater1, parameter2):
+    def write_parameters(self, f, parameter1, parameter2):
         if(parameter1 == 'ft'):#initializes the needed parameter to the value
             name1 = self.ft_tmp
         
@@ -226,8 +229,34 @@ class paramaters:
         elif(parameter2 == 'mr'):
             name2 = self.mr_tmp
             
-        
         f.write("%s\t%s \n" % (name1, name2))
+        
+    def check_interval(self, parameter, counter, interval):
+        if(parameter == 'ft'):
+            correct = ft_cn
+        elif(parameter == 'bt'):
+            correct = bt_cn
+        elif(parameter == 'r'):
+            correct = r_cn
+        elif(parameter == 'rr'):
+            correct = rr_cn
+        elif(parameter == 'm'):
+            correct = m_cn
+        elif(parameter == 'mr'):
+            correct = mr_cn
+        
+        
+        if(counter < correct and counter + interval > correct):
+            return True
+        else:
+            return False
+            
+            
+            
+    def run_system(self):
+        self.set_hist()
+        self.run_nbody()
+        self.write_parameters(self.file_name, self.parameter1, self.parameter2)
 
 def rebuild():#rebuilds nbody
     os.chdir(".")
@@ -353,97 +382,53 @@ def run_sweep(start1, end1, intv1, para1, start2, end2, intv2, para2):
     name2 = str(counter2)
    
     sweep_name = ""  #sweep name
-    
-    pipe_name = para1 + "_" + para2  #name of the files
-    
     os.system("mkdir " + folder + "parameter_sweeps" + sweep_name)
+    pipe_name = para1 + "_" + para2  #name of the files
+    hist_path = folder + pipe_name + "_hists/" + "arg_"
     data_vals   = folder + "parameter_sweeps" + sweep_name + "/" + pipe_name + "_vals.txt"
     f = open(data_vals, 'w')
     
-    ft_tmp = ft_c
-    bt_tmp = bt_c
-    r_tmp  = r_c
-    rr_tmp = rr_c
-    m_tmp  = m_c
-    mr_tmp = mr_c
     do_correct1 = False
     do_correct2 = False
     
+    para = parameters(ft_c, bt_c, r_c, rr_c, m_c, mr_c, hist_path, sweep_name, pipe_name, f, para1, para2)
+    
     while counter1 < end1 + intv1:  #this iterates over one parameter on the outside and another on the inside
+        para.set_parameter(para1, counter1)
+
+        
         counter2 = start2  #restart the inner parameter iteration from beginning
-        name2 = str(counter2)
-        
-        #checks which parameter needs to be updated for the outside loop and updates it
-        #also checks to see if the correct answer for the outside lies between current val and next increment, if so it sets do_correct1 to true
-        ft_tmp, bt_tmp, r_tmp, rr_tmp, m_tmp, mr_tmp, do_correct1 = para_init(para1, counter1, intv1, ft_tmp, bt_tmp, r_tmp, rr_tmp, m_tmp, mr_tmp )
-        
-        
         while counter2 < end2 + intv2:  #inner loop.
-            output_hist = folder + pipe_name + "_hists/" + "arg_"  #resets the output hist name
-           
-            #checks which parameter needs to be updated for the inner loop and updates it
-            #also checks if the correct answer for the inner lies between current val and next increment, if so sets do_correct2
-            ft_tmp, bt_tmp, r_tmp, rr_tmp, m_tmp, mr_tmp, do_correct2 = para_init(para2, counter2, intv2, ft_tmp, bt_tmp, r_tmp, rr_tmp, m_tmp, mr_tmp )
+            para.set_parameter(para2, counter2)
+            para.run_system()
             
+            do_correct2 = para.check_interval(para2, counter2, intv2)#check if correct answer falls between loop intervals
             
-            output_hist += ft_tmp + "_" + bt_tmp + "_" + r_tmp + "_" + rr_tmp + "_" + m_tmp + "_" + mr_tmp + ".hist"  #appends the current parameters to the output hist name
-            
-            nbody(output_hist, ft_tmp, bt_tmp, r_tmp, rr_tmp, m_tmp, mr_tmp, pipe_name, sweep_name)  #runs the sim with these parameters
-            
-            f.write("%s\t%s\n" % (name1, name2))  #writes the value to the value files.
-            
-            
-            if(do_correct2 == True):  #if the correct answer is between the interval for the inner loop, run the correct answer
-                write_correct(f, para2, name1)  #writes the correct answers to the value file
-                
-                output_hist = folder + pipe_name + "_hists/" + "arg_" + ft_tmp + "_" + bt_tmp + "_" + r_tmp + "_" + rr_tmp + "_" + m_tmp + "_" + mr_tmp + ".hist"  #sets the output hist name
-                nbody(output_hist, ft_tmp, bt_tmp, r_tmp, rr_tmp, m_tmp, mr_tmp, pipe_name, sweep_name)
-                
-                do_correct2 = False  #resets do_correct2
+            if(do_correct2):  #if the correct answer is between the interval for the inner loop, run the correct answer
+                para.set_correct(para2)
+                para.run_system()
             
             
             counter2 += intv2  #increment the inner loop value
-            name2 = str(counter2)
         
-        
-        if(do_correct1 == True):  #if the outer loop correct answer was found in the interval
-            name1_save = name1  #save the current iterated value for the outer loop
-            
-            ft_tmp, bt_tmp, r_tmp, rr_tmp, m_tmp, mr_tmp, name1 = correct_set(para1, ft_tmp, bt_tmp, r_tmp, rr_tmp, m_tmp, mr_tmp)  #set the correct values for the outer loop
+        do_correct1 = para.check_interval(para1, counter1, intv1)
+        if(do_correct1):  #if the outer loop correct answer was found in the interval
+            para.set_correct(para1)
                 
             
             counter2 = start2  #run the same inner loop as above
-            name2 = str(counter2)
             while counter2 < end2 + intv2:
-                output_hist = folder + pipe_name + "_hists/" + "arg_"  #resets the output hist name
+                para.set_parameter(para2, counter2)
+                para.run_system()
                 
-                #checks which parameter needs to be updated for the inner loop and updates it
-                #also checks if the correct answer for the inner lies between current val and next increment, if so sets do_correct2
-                ft_tmp, bt_tmp, r_tmp, rr_tmp, m_tmp, mr_tmp, do_correct2 = para_init(para2, counter2, intv2, ft_tmp, bt_tmp, r_tmp, rr_tmp, m_tmp, mr_tmp )
-                
-                
-                output_hist += ft_tmp + "_" + bt_tmp + "_" + r_tmp + "_" + rr_tmp + "_" + m_tmp + "_" + mr_tmp + ".hist"  #appends the current parameters to the output hist name
-                nbody(output_hist, ft_tmp, bt_tmp, r_tmp, rr_tmp, m_tmp, mr_tmp, pipe_name, sweep_name)
-                
-                f.write("%s\t%s\n" % (name1, name2))  #writes the correct values to the value file
-                
-                
-                if(do_correct2 == True):  #if the correct answer for the inner loop was found 
-                    write_correct(f, para2, name1)  #write the correct answers to the value file
-                    
-                    output_hist = folder + pipe_name + "_hists/" + "arg_" + ft_c + "_" + bt_c + "_" + r_c + "_" + rr_c + "_" + m_c + "_" + mr_c + ".hist"  #sets the output hist name
-                    nbody(output_hist, ft_c, bt_c, r_c, rr_c, m_c, mr_c, pipe_name, sweep_name)
-                    do_correct2 = False  #resets do_correct2
-
+                do_correct2 = para.check_interval(para2, counter2, intv2)
+                if(do_correct2):  #if the correct answer for the inner loop was found 
+                    para.set_correct(para2)
+                    para.run_system()
                 
                 counter2 += intv2  #iterates the inner loop value
-                name2 = str(counter2)
-            
-            name1 = name1_save  #once the correct value was put in, reset the counter to previous value so that iteration continues
-            do_correct1 = False  #reset do_correct1
         
         counter1 += intv1  #iterates the outer loop value
-        name1 = str(counter1)
     f.close()#close the files
     return 0
 
@@ -550,72 +535,51 @@ def random_iteration_sweep(start1, end1, N1, para1, start2, end2, N2, para2):
     counter2 = 0.0
     #sweep name
     sweep_name = "_2d_rand_iter"
+    os.system("mkdir " + folder + "parameter_sweeps" + sweep_name)
     #name of the files
     pipe_name = para1 + "_" + para2
-    
-    os.system("mkdir " + folder + "parameter_sweeps" + sweep_name)
+    hist_path = folder + pipe_name + "_hists/" + "arg_"
     data_vals   = folder + "parameter_sweeps" + sweep_name + "/" + pipe_name + "_vals.txt"
     f = open(data_vals, 'w')
     
-    ft_tmp = ft_c
-    bt_tmp = bt_c
-    r_tmp  = r_c
-    rr_tmp = rr_c
-    m_tmp  = m_c
-    mr_tmp = mr_c
+    #initialize the parameter class with the correct values
+    para = parameters(ft_c, bt_c, r_c, rr_c, m_c, mr_c, hist_path, sweep_name, pipe_name, f, para1, para2)
     
      #this iterates over one parameter on the outside and another on the inside
     while counter1 < N1:
-        if(counter1 == 0.0):#since this has random iteration, put the correct value first
-            ft_tmp, bt_tmp, r_tmp, rr_tmp, m_tmp, mr_tmp, name1 = correct_set(para1, ft_tmp, bt_tmp, r_tmp, rr_tmp, m_tmp, mr_tmp)  #sets the correct value for the outerloop
-
+        
+        if(counter1 == 0.0):#since this has random iteration, put the para1 correct value first
             counter2 = 0.0#reset the inner loop counter
+            
             while counter2 < N2:#runs the inner loop iteration
+                
                 if(counter2 == 0.0):#put the correct value for the inner loop first
-                    ft_tmp, bt_tmp, r_tmp, rr_tmp, m_tmp, mr_tmp, name2 = correct_set(para2, ft_tmp, bt_tmp, r_tmp, rr_tmp, m_tmp, mr_tmp)  #sets the correct value for the inner loop
-                    output_hist = folder + pipe_name + "_hists/" + "arg_" + ft_c + "_" + bt_c + "_" + r_c + "_" + rr_c + "_" + m_c + "_" + mr_c + ".hist"  #sets the name of the output hist
-                    nbody(output_hist, ft_c, bt_c, r_c, rr_c, m_c, mr_c, pipe_name, sweep_name)
-                    f.write("%s\t%s\n" % (name1, name2))  #write values to value file
+                    para.run_system()#put para2 correct value first
                 
-                output_hist = folder + pipe_name + "_hists/" + "arg_"  #reset output file name
                 
-                name2 = random.uniform(0.0, 1.0) * (end2 - start2) + start2  #randomly select a value in the sweep range of inner loop parameter
-                name2 = str(name2)
+                value = random.uniform(0.0, 1.0) * (end2 - start2) + start2  #randomly select a value in the sweep range of inner loop parameter
                 
-                ft_tmp, bt_tmp, r_tmp, rr_tmp, m_tmp, mr_tmp = para_init_rand(para2, name2, ft_tmp, bt_tmp, r_tmp, rr_tmp, m_tmp, mr_tmp )  #set the values for the correct parameter for the inner loop
-                
-                output_hist += ft_tmp + "_" + bt_tmp + "_" + r_tmp + "_" + rr_tmp + "_" + m_tmp + "_" + mr_tmp + ".hist" #append the values to the hist name
-                nbody(output_hist, ft_tmp, bt_tmp, r_tmp, rr_tmp, m_tmp, mr_tmp, pipe_name, sweep_name)
-                f.write("%s\t%s\n" % (name1, name2)) #write values to value file
+                para.set_parameter(para2, value)#set the second parameter to a random parameter
+                para.run_system()#sets the name of the histogram, runs the nbody and writes the parameters used
                 
                 counter2 += 1 #iterate counter. want a certain number of points
         
-        name1 = random.uniform(0.0, 1.0) * (end1 - start1) + start1  #after putting correct answer, randomly select value from sweep range for outer loop parameter
-        name1 = str(name1)
+        value = random.uniform(0.0, 1.0) * (end1 - start1) + start1  #after putting correct answer, randomly select value from sweep range for outer loop parameter
         
-        ft_tmp, bt_tmp, r_tmp, rr_tmp, m_tmp, mr_tmp = para_init_rand(para1, name1, ft_tmp, bt_tmp, r_tmp, rr_tmp, m_tmp, mr_tmp )  #set the values for the correct parameter for the inner loop
+        para.set_parameter(para1, value)
         
         counter2 = 0.0  #reset the counter for the inner loop
         while counter2 < N2:
             if(counter2 == 0.0):#put the correct value for the inner loop first
-                ft_tmp, bt_tmp, r_tmp, rr_tmp, m_tmp, mr_tmp, name2 = correct_set(para2, ft_tmp, bt_tmp, r_tmp, rr_tmp, m_tmp, mr_tmp)
+                para.set_correct(para2)#set the para2 to correct value
                 
-                output_hist = folder + pipe_name + "_hists/" + "arg_" + ft_tmp + "_" + bt_tmp + "_" + r_tmp + "_" + rr_tmp + "_" + m_tmp + "_" + mr_tmp + ".hist" #set the hist name
-                nbody(output_hist, ft_tmp, bt_tmp, r_tmp, rr_tmp, m_tmp, mr_tmp, pipe_name, sweep_name)
-                
-                f.write("%s\t%s\n" % (name1, name2)) #write values to value file
-                
+                para.run_system()
             
-            output_hist = folder + pipe_name + "_hists/" + "arg_" #reset the name of the hists
-            
-            name2 = random.uniform(0.0, 1.0) * (end2 - start2) + start2 #randomly select a value within the sweep range for the inner loop
-            name2 = str(name2)
+            value = random.uniform(0.0, 1.0) * (end2 - start2) + start2 #randomly select a value within the sweep range for the inner loop
            
-            ft_tmp, bt_tmp, r_tmp, rr_tmp, m_tmp, mr_tmp = para_init_rand(para2, name2, ft_tmp, bt_tmp, r_tmp, rr_tmp, m_tmp, mr_tmp )  #assign values to the correct parameter for the inner loop
-                
-            output_hist += ft_tmp + "_" + bt_tmp + "_" + r_tmp + "_" + rr_tmp + "_" + m_tmp + "_" + mr_tmp + ".hist"
-            nbody(output_hist, ft_tmp, bt_tmp, r_tmp, rr_tmp, m_tmp, mr_tmp, pipe_name, sweep_name)
-            f.write("%s\t%s\n" % (name1, name2))
+            para.set_parameter(para2, value)
+            para.run_system()
+            
             counter2 += 1
         counter1 +=1
         
@@ -624,10 +588,10 @@ def random_iteration_sweep(start1, end1, N1, para1, start2, end2, N2, para2):
 
 
 def truly_random_sweep(start1, end1, N1, para1, start2, end2, N2, para2):
-    counter1 = 0.0
-    counter2 = 0.0
+    counter = 0.0
     #sweep name
     sweep_name = "_2d_rand_iter"
+    os.system("mkdir " + folder + "parameter_sweeps" + sweep_name)
     #name of the files
     pipe_name = para1 + "_" + para2
     hist_path = folder + pipe_name + "_hists/" + "arg_"
@@ -636,19 +600,10 @@ def truly_random_sweep(start1, end1, N1, para1, start2, end2, N2, para2):
     data_vals   = folder + "parameter_sweeps" + sweep_name + "/" + pipe_name + "_vals.txt"
     f = open(data_vals, 'w')
     
-    ft_tmp = ft_c
-    bt_tmp = bt_c
-    r_tmp  = r_c
-    rr_tmp = rr_c
-    m_tmp  = m_c
-    mr_tmp = mr_c
-    
-    N = (N1 * N2) / 3
-    para = parameter(ft_c, bt_c, r_c, rr_c, m_c, mr_c, hist_path, sweep_name, pipe_name)
+    N = (N1 * N2) 
+    para = parameters(ft_c, bt_c, r_c, rr_c, m_c, mr_c, hist_path, sweep_name, pipe_name, f, para1, para2)
     #run the correct answer first
-    para.set_hist()
-    para.run_nbody()
-    para.write_parameters(f, para1, para1)
+    para.run_system()
     
     #this iterates over one parameter on the outside and another on the inside
     while counter < N:
@@ -662,23 +617,16 @@ def truly_random_sweep(start1, end1, N1, para1, start2, end2, N2, para2):
         
         #set first parameter to random value keeping second as correct
         para.set_parameter(para1, value1)
-        para.set_hist()
-        para.run_nbody()
-        para.write_parameters(f, para1, para2)
+        para.run_system()
         
         
         #set both parameters to random numbers
         para.set_parameter(para2, value2)
-        para.set_hist()
-        para.run_nbody()
-        para.write_parameters(f, para1, para2)
-        
+        para.run_system()
         
         #set the first to correct and the second to the random number
         para.set_correct(para1)
-        para.set_hist()
-        para.run_nbody()
-        para.write_parameters(f, para1, para2)
+        para.run_system()
         
         counter += 1
         
@@ -779,8 +727,13 @@ def main():
             
         if(run_m_v_mr  ):
             random_iteration_sweep(m_s,  m_e,  m_N,  'm',  mr_s, mr_e, mr_N, 'mr')
-            
-    tmp_sweep_correction(r_s,  r_e,  r_N,  'r',  mr_s, mr_e, mr_N, 'mr')
+     
+     
+     
+    if(run_truly_random_iter):
+        truly_random_sweep(rr_s, rr_e, rr_N, 'rr', mr_s, mr_e, mr_N, 'mr')
+        
+    #tmp_sweep_correction(r_s,  r_e,  r_N,  'r',  mr_s, mr_e, mr_N, 'mr')
 main()
             
         
